75e3c023823eb9079c78b2653331b417733a9a67
vpopolitov <tiermes@yandex.ru>
2015-01-19 00:05:12 +0400

    Refactoring: route should build request and adapter store some context info

--- lib/api_caller/adapter.rb
+++ lib/api_caller/adapter.rb
@@ -14,7 +14,7 @@ module ApiCaller
       def build_request(route_name, params = {})
         route = routes[route_name]
         raise ApiCaller::Error::MissingRoute, route_name unless route
-        url = process_route(route, params)
+        url = route.process_route(route, { base_url: base_url, params: params })
         Request.new(route.http_verb, url)
       end
 
@@ -30,35 +30,6 @@ module ApiCaller
 
       def base_url
         @base_url ||= ''
-      end
-
-      def url_pattern
-        /(?<sep>&)?((?<key>\w+)=)?:(?<val>\w+)/
-      end
-
-      def process_route(route, params = {})
-        pattern = route.pattern
-        url = pattern.gsub(url_pattern) do
-          val = $~[:val].to_sym
-          key = $~[:key]
-          sep = $~[:sep]
-          query_param = params.delete val
-
-          if key
-            if query_param
-              "#{sep}#{key}=#{query_param}"
-            else
-              ''
-            end
-          else
-            if query_param
-              query_param
-            else
-              raise ApiCaller::Error::MissingResourceParameterValue, val
-            end
-          end
-        end
-        base_url.empty? ? url : "#{base_url}/#{url}"
       end
     end
   end
--- lib/api_caller/route.rb
+++ lib/api_caller/route.rb
@@ -6,5 +6,39 @@ module ApiCaller
       @pattern = params[:pattern]
       @http_verb  = params[:http_verb]
     end
+
+    def process_route(route, context = {})
+      params  = context[:params]
+      base_url = context[:base_url]
+
+      pattern = route.pattern
+      url = pattern.gsub(url_pattern) do
+        val = $~[:val].to_sym
+        key = $~[:key]
+        sep = $~[:sep]
+        query_param = params.delete val
+
+        if key
+          if query_param
+            "#{sep}#{key}=#{query_param}"
+          else
+            ''
+          end
+        else
+          if query_param
+            query_param
+          else
+            raise ApiCaller::Error::MissingResourceParameterValue, val
+          end
+        end
+      end
+      base_url.empty? ? url : "#{base_url}/#{url}"
+    end
+
+    private
+
+    def url_pattern
+      /(?<sep>&)?((?<key>\w+)=)?:(?<val>\w+)/
+    end
   end
 end
